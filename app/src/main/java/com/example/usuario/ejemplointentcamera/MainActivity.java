c9c427e1e73f7786ad8e57e3fcc11d4cecfca7de
mpgprofe <mpgprofe@gmail.com>
2018-12-03 11:00:56 +0100

    Subida a un servidor con un script php: <?php move_uploaded_file($_FILES["pepitogrillo"]["tmp_name"], "../subidas/".$_FILES["pepitogrillo"]["name"].".png"); echo "Todo Correcto."; ?>

--- app/src/main/java/com/example/usuario/ejemplointentcamera/MainActivity.java
+++ app/src/main/java/com/example/usuario/ejemplointentcamera/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Environment;
 import android.os.StrictMode;
 import android.provider.MediaStore;
@@ -14,13 +15,22 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -34,7 +44,8 @@ public class MainActivity extends AppCompatActivity {
     static final int VENGO_DE_LA_CAMARA = 1;
     static final int VENGO_DE_LA_CAMARA_CON_FICHERO = 2;
     static final int PEDI_PERMISOS_DE_ESCRITURA = 2;
-Button captura, captura2;
+
+Button captura, captura2, subir;
 ImageView imageViewFoto;
 String rutaFotoActual;
     @Override
@@ -46,6 +57,7 @@ String rutaFotoActual;
         captura = findViewById(R.id.buttonCaptura);
         captura2 = findViewById(R.id.buttonCaptura2);
         imageViewFoto = findViewById(R.id.imageViewFoto);
+        subir = findViewById(R.id.buttonSubir);
 
         captura.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -71,6 +83,14 @@ String rutaFotoActual;
 
             }
         });
+
+        subir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                new SubirFichero().execute(rutaFotoActual);
+            }
+        });
     }
 
     @Override
@@ -83,6 +103,7 @@ String rutaFotoActual;
         }else if ((requestCode == VENGO_DE_LA_CAMARA_CON_FICHERO) && (resultCode == RESULT_OK)){
             //He hecho la foto y la he guardado, así que la foto estará en  rutaFotoActual
             imageViewFoto.setImageBitmap(BitmapFactory.decodeFile(rutaFotoActual));
+
 
         }
 
@@ -162,4 +183,103 @@ String rutaFotoActual;
            //Pondría aquí más "case" si tuviera que pedir más permisos.
         }
     }
+
+    private class SubirFichero extends AsyncTask<Object, Integer, Void>{
+
+        String scriptSubida = "http://192.168.1.48/scripts/subidaFichero.php";
+        URL connectURL;
+        FileInputStream fis;
+
+
+        @Override
+        protected void onPreExecute() {
+            try {
+                connectURL = new URL(scriptSubida);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        @Override
+        protected Void doInBackground(Object... objects) {
+            String nombreFichero = objects[0].toString();
+
+            try {
+                fis = new FileInputStream(nombreFichero);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+
+            String lineEnd = "\r\n";
+            String twoHyphens = "--";
+            String boundary = "*****";
+
+
+            HttpURLConnection conn = null;
+            try {
+                conn = (HttpURLConnection) connectURL.openConnection();
+
+
+            conn.setDoInput(true);
+            conn.setDoOutput(true);
+            conn.setUseCaches(false);
+
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Connection","Keep-Alive");
+            conn.setRequestProperty("Content-Type","multipart/form-data;boundary="+boundary);
+            DataOutputStream dos = new DataOutputStream(conn.getOutputStream());
+
+            dos.writeBytes(twoHyphens+boundary+lineEnd);
+            dos.writeBytes("Content-Disposition: form-data; name=\"pepitogrillo\";filename=\"ficheroPrueba\""+lineEnd);
+            dos.writeBytes(lineEnd);
+
+            int bytesAvailabe = fis.available();
+            int maxBufferSize =  1024;
+            int bufferSize = Math.min(bytesAvailabe,maxBufferSize);
+            byte[] buffer = new byte[bufferSize];
+            int bytesRead = fis.read(buffer,0, bufferSize);
+            int total = fis.available();
+            int enviados = 0;
+            int progress = 0;
+            while(bytesRead>0){
+                dos.write(buffer,0,bufferSize);
+                bytesAvailabe= fis.available();
+                bufferSize = Math.min(bytesAvailabe,maxBufferSize);
+                bytesRead = fis.read(buffer,0, bufferSize);
+            }
+            dos.writeBytes(lineEnd);
+            dos.writeBytes(twoHyphens+boundary+twoHyphens+lineEnd);
+            dos.writeBytes(lineEnd);
+            dos.writeBytes("--"+boundary+"--"+lineEnd);
+
+            fis.close();
+            dos.flush();
+
+            String responseString;
+            int responseCode = conn.getResponseCode();
+                Log.d("EJEMPLOCAMERA", "Código respuesta: "+responseCode);
+                InputStream inStream = null;
+
+                inStream = conn.getInputStream();
+                ByteArrayOutputStream out = new ByteArrayOutputStream(Math.max(1024, conn.getContentLength()));
+                byte[] buffer2 = new byte[1024];
+                int bytesRead2 = 0;
+
+                while((bytesRead2 = inStream.read(buffer2))>0){
+                    out.write(buffer2,0,bytesRead2);
+                }
+                responseString = out.toString("UTF-8");
+                Log.d("EJEMPLOCAMERA", "Respuesta: "+responseString);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+
+            return null;
+        }
+    }
+
 }
